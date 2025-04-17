ab83109a39290849454566d01a93bf7ce6e508f8
default <devin.boucher@stu.ocsb.ca>
2025-04-17 16:35:17 +0000

    added html and js

--- js/script.js
+++ js/script.js
@@ -1,5 +1,30 @@
 // Copyright (c) 2020 Mr. Coxall All rights reserved
 //
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: Devin Boucher
+// Created on: April 2025
 // This file contains the JS functions for index.html
+
+"use strict"
+const randomNumber = Math.floor(Math.random() * 6) + 1
+
+/**
+ * Checks random number
+ */
+function checkNumber() {
+  // Get input values
+  const userNumber = parseFloat(document.getElementById("user-Number").value)
+
+  // process
+if(userNumber === randomNumber){
+  // output
+  document.getElementById('result').innerHTML =
+  '<p>You have guessed the correct number</p>'
+}
+
+if (userNumber !== randomNumber) {
+  // output
+    document.getElementById('result').innerHTML =
+    '<p>You have guessed the wrong number' +
+    '<br/>The correct number was: ' + randomNumber + '</p>'
+  }
+}
